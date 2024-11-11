da4e9295cc8770074386b8524bfa2abadf630d94
yzeybek <179846319+yzeyb@users.noreply.github.com>
2024-11-12 00:42:26 +0300

    added keybindings

--- setup.sh
+++ setup.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/bash
 
+# Function definations
+add_keybinding() {
+    local new_keybinding="$1"
+    local keybindings_file="$HOME/.config/Code/User/keybindings.json"
+
+    if [[ ! -f "$keybindings_file" ]]; then
+        echo "[]" > "$keybindings_file"
+    fi
+
+    local content
+    content=$(grep -v '^//' "$keybindings_file" | tr -d '\n' | tr -d '\r')
+
+    content=$(echo "$content" | tr -d '\n' | tr -d '\r')
+
+    if [[ "$content" == "[]" ]]; then
+        echo "[$new_keybinding]" > "$keybindings_file"
+    else
+        if [[ "$content" =~ \]$ ]]; then
+            content="${content%?}"
+            echo "$content,$new_keybinding]" > "$keybindings_file"
+        else
+            echo "$content$new_keybinding]" > "$keybindings_file"
+        fi
+    fi
+}
+
 # Variables
 APP_NAME=Ubuntu-VSCode
 
@@ -14,6 +40,12 @@ FOLDER_PATH=$DIR_PATH/$FOLDER_NAME
 
 ICON_NAME=code.png
 ICON_PATH=$DIR_PATH/resources/app/resources/linux/$ICON_NAME
+
+KEY_C='{"key": "meta+c","command": "workbench.action.terminal.copySelection","when": "terminalTextSelectedInFocused || terminalFocus && terminalHasBeenCreated && terminalTextSelected || terminalFocus && terminalProcessSupported && terminalTextSelected || terminalFocus && terminalTextSelected && terminalTextSelectedInFocused || terminalHasBeenCreated && terminalTextSelected && terminalTextSelectedInFocused || terminalProcessSupported && terminalTextSelected && terminalTextSelectedInFocused"}'
+KEY_C_REMOVE='{"key": "ctrl+shift+c","command": "-workbench.action.terminal.copySelection","when": "terminalTextSelectedInFocused || terminalFocus && terminalHasBeenCreated && terminalTextSelected || terminalFocus && terminalProcessSupported && terminalTextSelected || terminalFocus && terminalTextSelected && terminalTextSelectedInFocused || terminalHasBeenCreated && terminalTextSelected && terminalTextSelectedInFocused || terminalProcessSupported && terminalTextSelected && terminalTextSelectedInFocused"}'
+
+KEY_V='{"key": "meta+v","command": "workbench.action.terminal.paste","when": "terminalFocus && terminalHasBeenCreated || terminalFocus && terminalProcessSupported"}'
+KEY_V_REMOVE='{"key": "ctrl+shift+v","command": "-workbench.action.terminal.paste","when": "terminalFocus && terminalHasBeenCreated || terminalFocus && terminalProcessSupported"}'
 
 # Tar file operations
 mkdir $DIR_PATH
@@ -50,3 +82,9 @@ echo "export PATH=\$PATH:$DIR_PATH/bin" >> ~/.zshrc
 echo "export PATH=\$PATH:$DIR_PATH/bin" >> ~/.bashrc
 source ~/.bashrc
 source ~/.zshrc
+
+# Key bindings
+add_keybinding "$KEY_C"
+add_keybinding "$KEY_C_REMOVE"
+add_keybinding "$KEY_V"
+add_keybinding "$KEY_V_REMOVE"
