669b6f66aad32b9839f1dad3826bc643145827fd
yzeybek <179846319+yzeyb@users.noreply.github.com>
2024-12-03 13:25:08 +0300

    updated uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/bash
 
 # Variables
-APP_NAME=Wcode
-
 DIR_NAME=Wcode
 DIR_PATH=$HOME/$DIR_NAME
 
-# Deletions
+DESKTOP_NAME=code.desktop
+URL_DESKTOP_NAME=code-url-handler.desktop
+DESKTOP_PATH=$HOME/.local/share/applications/$DESKTOP_NAME
+URL_DESKTOP_PATH=$HOME/.local/share/applications/$URL_DESKTOP_NAME
 
+# Deletions
 rm -rf $DIR_PATH
-rm -f ~/.local/share/applications/$APP_NAME.desktop
+rm -f $DESKTOP_PATH
+rm -f $URL_DESKTOP_PATH
