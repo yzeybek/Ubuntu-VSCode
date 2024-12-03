7ea1985beceb82137ab5725d628552fffa0d44e3
yzeybek <179846319+yzeyb@users.noreply.github.com>
2024-12-03 13:21:38 +0300

    desktop files refreshed

--- install.sh
+++ install.sh
@@ -1,31 +1,5 @@
 #!/usr/bin/bash
 
-# Function definations
-add_keybinding() {
-    local new_keybinding="$1"
-    local keybindings_file="$HOME/.config/Code/User/keybindings.json"
-
-    if [[ ! -f "$keybindings_file" ]]; then
-        echo "[]" > "$keybindings_file"
-    fi
-
-    local content
-    content=$(grep -v '^//' "$keybindings_file" | tr -d '\n' | tr -d '\r')
-
-    content=$(echo "$content" | tr -d '\n' | tr -d '\r')
-
-    if [[ "$content" == "[]" ]]; then
-        echo "[$new_keybinding]" > "$keybindings_file"
-    else
-        if [[ "$content" =~ \]$ ]]; then
-            content="${content%?}"
-            echo "$content,$new_keybinding]" > "$keybindings_file"
-        else
-            echo "$content$new_keybinding]" > "$keybindings_file"
-        fi
-    fi
-}
-
 # Variables
 APP_NAME=Wcode
 
@@ -41,13 +15,10 @@ FOLDER_PATH=$DIR_PATH/$FOLDER_NAME
 ICON_NAME=code.png
 ICON_PATH=$DIR_PATH/resources/app/resources/linux/$ICON_NAME
 
-KEY_C='{"key": "meta+c","command": "workbench.action.terminal.copySelection","when": "terminalTextSelectedInFocused || terminalFocus && terminalHasBeenCreated && terminalTextSelected || terminalFocus && terminalProcessSupported && terminalTextSelected || terminalFocus && terminalTextSelected && terminalTextSelectedInFocused || terminalHasBeenCreated && terminalTextSelected && terminalTextSelectedInFocused || terminalProcessSupported && terminalTextSelected && terminalTextSelectedInFocused"}'
-KEY_C_REMOVE='{"key": "ctrl+shift+c","command": "-workbench.action.terminal.copySelection","when": "terminalTextSelectedInFocused || terminalFocus && terminalHasBeenCreated && terminalTextSelected || terminalFocus && terminalProcessSupported && terminalTextSelected || terminalFocus && terminalTextSelected && terminalTextSelectedInFocused || terminalHasBeenCreated && terminalTextSelected && terminalTextSelectedInFocused || terminalProcessSupported && terminalTextSelected && terminalTextSelectedInFocused"}'
-
-KEY_V='{"key": "meta+v","command": "workbench.action.terminal.paste","when": "terminalFocus && terminalHasBeenCreated || terminalFocus && terminalProcessSupported"}'
-KEY_V_REMOVE='{"key": "ctrl+shift+v","command": "-workbench.action.terminal.paste","when": "terminalFocus && terminalHasBeenCreated || terminalFocus && terminalProcessSupported"}'
-
-KEY_ADD='{"key": "meta+g","command": "workbench.action.terminal.sendSequence","args": {"text": "addignore\u000D"}}'
+DESKTOP_NAME=code.desktop
+URL_DESKTOP_NAME=code-url-handler.desktop
+DESKTOP_PATH=$HOME/.local/share/applications/$DESKTOP_NAME
+URL_DESKTOP_PATH=$HOME/.local/share/applications/$URL_DESKTOP_NAME
 
 # Tar file operations
 mkdir $DIR_PATH
@@ -57,25 +28,54 @@ cp -r $FOLDER_PATH/* $DIR_PATH
 rm -rf $FOLDER_PATH $TAR_PATH
 rm -f $ICON_PATH
 
-# Get image
-curl https://raw.githubusercontent.com/yzeybek/Wcode/refs/heads/main/code.png -o $ICON_PATH
-
 # Desktop entry
 echo "[Desktop Entry]
-Name=$APP_NAME
-Comment=Programming Text Editor
-Exec=$DIR_PATH/bin/code
-Icon=$ICON_PATH
-Terminal=false
+Name=Visual Studio Code
+Comment=Code Editing. Redefined.
+GenericName=Text Editor
+Exec=$DIR_PATH/bin/code %F
+Icon=vscode
 Type=Application
-Categories=Development" > ~/.local/share/applications/$APP_NAME.desktop
-chmod +x ~/.local/share/applications/$APP_NAME.desktop
+StartupNotify=false
+StartupWMClass=Code
+Categories=TextEditor;Development;IDE;
+MimeType=application/x-code-workspace;
+Actions=new-empty-window;
+Keywords=vscode;
+
+[Desktop Action new-empty-window]
+Name=New Empty Window
+Name[de]=Neues leeres Fenster
+Name[es]=Nueva ventana vacía
+Name[fr]=Nouvelle fenêtre vide
+Name[it]=Nuova finestra vuota
+Name[ja]=新しい空のウィンドウ
+Name[ko]=새 빈 창
+Name[ru]=Новое пустое окно
+Name[zh_CN]=新建空窗口
+Name[zh_TW]=開新空視窗
+Exec=$DIR_PATH/bin/code --new-window %F
+Icon=vscode" > $DESKTOP_PATH
+echo "[Desktop Entry]
+Name=Visual Studio Code - URL Handler
+Comment=Code Editing. Redefined.
+GenericName=Text Editor
+Exec=$DIR_PATH/bin/code --open-url %U
+Icon=vscode
+Type=Application
+NoDisplay=true
+StartupNotify=true
+Categories=Utility;TextEditor;Development;IDE;
+MimeType=x-scheme-handler/vscode;
+Keywords=vscode;" > $URL_DESKTOP_PATH
+chmod +x $DESKTOP_PATH
+chmod +x $URL_DESKTOP_PATH
 update-desktop-database ~/.local/share/applications
 
 # Gnome shell update
 current_favorites=$(gsettings get org.gnome.shell favorite-apps)
 current_favorites_cleaned=$(echo "$current_favorites" | sed "s/^\[//" | sed "s/\]//")
-new_favorites="$current_favorites_cleaned, '$APP_NAME.desktop'"
+new_favorites="$current_favorites_cleaned, '$DESKTOP_NAME'"
 new_favorites_wrapped="[$new_favorites]"
 gsettings set org.gnome.shell favorite-apps "$new_favorites_wrapped"
 
@@ -83,45 +83,5 @@ gsettings set org.gnome.shell favorite-apps "$new_favorites_wrapped"
 echo "export PATH=\$PATH:$DIR_PATH/bin" >> ~/.zshrc
 echo "export PATH=\$PATH:$DIR_PATH/bin" >> ~/.bashrc
 
-# Add ignore
-echo "alias addignore=\"echo '# Add Yours here
-
-
-# General
-a.out
-.vscode
-.DS_Store
-main.c
-test
-data
-.gitignore
-**/*.o
-*.o
-*.swp
-**/*.swp
-' > .gitignore\"" >> ~/.bashrc
-echo "alias addignore=\"echo '# Add Yours here
-
-
-# General
-a.out
-.vscode
-.DS_Store
-main.c
-test
-data
-.gitignore
-**/*.o
-*.o
-*.swp
-**/*.swp
-' > .gitignore\"" >> ~/.zshrc
 source ~/.bashrc
 source ~/.zshrc
-
-# Key bindings
-add_keybinding "$KEY_C"
-add_keybinding "$KEY_C_REMOVE"
-add_keybinding "$KEY_V"
-add_keybinding "$KEY_V_REMOVE"
-add_keybinding "$KEY_ADD"
